7458fe278862766fd6554190c9df69221837f9a3
acheco <80710861+acheco@users.noreply.github.com>
2026-02-15 15:57:07 -0400

    Add Sidebar component and integrate with AppLayout

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,4 +1,5 @@
 import MobileNav from '@/components/mobile-nav';
+import Sidebar from '@/components/sidebar';
 import { dashboard } from '@/routes';
 import { usePage } from '@inertiajs/react';
 import {
@@ -9,7 +10,8 @@ import {
   ReceiptIcon,
   TipJarIcon,
 } from '@phosphor-icons/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { cn } from '../../lib/utils';
 
 const navigationItems = [
   {
@@ -46,10 +48,28 @@ const navigationItems = [
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { url } = usePage();
+  const appName = usePage<{ name: string }>().props.name;
+
+  const [isMinimized, setIsMinimized] = useState(false);
 
   return (
-    <div className="grid h-screen grid-cols-1 grid-rows-[1fr_52px] bg-beige-100 md:grid-rows-[1fr_74px]">
-      <main className="">{children}</main>
+    <div className="grid h-screen grid-cols-1 grid-rows-[1fr_52px] bg-beige-100 md:grid-rows-[1fr_74px] lg:grid-rows-1">
+      <div
+        className={cn(
+          'grid',
+          isMinimized ? 'grid-cols-[80px_1fr]' : 'grid-cols-[300px_1fr]',
+        )}
+      >
+        <Sidebar
+          appName={appName}
+          navigationItems={navigationItems}
+          url={url}
+          isMinimized={isMinimized}
+          setIsMinimized={setIsMinimized}
+        />
+
+        <main className="px-10 py-8">{children}</main>
+      </div>
       <footer className="grid content-end bg-grey-900 sm:px-2 md:px-5 lg:hidden">
         <MobileNav navItems={navigationItems} currentPath={url} />
       </footer>
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,12 +1,9 @@
 import AppLayout from '@/layouts/app-layout';
-import { logout } from '@/routes';
-import { Link } from '@inertiajs/react';
 
 export default function dashboard({}) {
   return (
     <AppLayout>
       <h1>Dashboard</h1>
-      <Link href={logout()}>Logout</Link>
     </AppLayout>
   );
 }
